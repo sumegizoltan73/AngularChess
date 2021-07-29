50c7007953954a7fed16f0eef0c2a1814ed3dda8
Sümegi Zoltán <sumegizoltan73@gmail.com>
2021-07-29 19:55:29 +0200

    Inited board

--- angular-chess/src/app/chess/chess.component.ts
+++ angular-chess/src/app/chess/chess.component.ts
@@ -5,6 +5,12 @@ export interface IFigure {
   color: string;
 }
 
+export interface ICell {
+  x: number;
+  y: number;
+  figure: IFigure | null;
+}
+
 @Component({
   selector: 'app-chess',
   templateUrl: './chess.component.html',
@@ -15,7 +21,11 @@ export class ChessComponent implements OnInit {
   x: string[] = ['a','b','c','d','e','f','g','h'];
   y: string[] = ['8','7','6','5','4','3','2','1'];
 
-  constructor() { }
+  private board: ICell[] = [];
+
+  constructor() { 
+    this.fillBoard();
+  }
 
   ngOnInit(): void {
   }
@@ -37,10 +47,120 @@ export class ChessComponent implements OnInit {
   }
 
   private getFigure(x: number, y: number): IFigure | null {
-    // test
-    if ((8 - y) === 7)
-      return { name: 'person', color: 'black'};
+    const cell = this.board.find(function (el) {
+      return el.x === x &&
+             el.y === (y);
+    });
 
-    return null;
+    return (cell && cell.figure)? cell.figure : null;
+  }
+
+  private fillBoard(): void {
+    //init white figures
+    for (let i = 0; i < this.x.length; i++) {
+      const element = this.x[i];
+      // row 2 - Peasant
+      this.board.push({
+        x: i,
+        y: (8 - 2),
+        figure: { name: "person", color: "white" }
+      });
+
+      // row 1
+      if (element === 'a' || element === 'h') {
+        // Bastion
+        this.board.push({
+          x: i,
+          y: (8 - 1),
+          figure: { name: "settings", color: "white" }
+        });
+      }
+      else if (element === 'b' || element === 'g') {
+        // Colt
+        this.board.push({
+          x: i,
+          y: (8 - 1),
+          figure: { name: "savings", color: "white" }
+        });
+      }
+      else if (element === 'c' || element === 'f') {
+        // Runner
+        this.board.push({
+          x: i,
+          y: (8 - 1),
+          figure: { name: "account_circle", color: "white" }
+        });
+      }
+      else if (element === 'd') {
+        // Queen
+        this.board.push({
+          x: i,
+          y: (8 - 1),
+          figure: { name: "grade", color: "white" }
+        });
+      }
+      else if (element === 'e') {
+        // King
+        this.board.push({
+          x: i,
+          y: (8 - 1),
+          figure: { name: "engineering", color: "white" }
+        });
+      }
+    }
+
+    
+    //init black figures
+    for (let i = 0; i < this.x.length; i++) {
+      const element = this.x[i];
+      // row 7 - Peasant
+      this.board.push({
+        x: i,
+        y: (8 - 7),
+        figure: { name: "person", color: "black" }
+      });
+
+      // row 8
+      if (element === 'a' || element === 'h') {
+        // Bastion
+        this.board.push({
+          x: i,
+          y: (8 - 8),
+          figure: { name: "settings", color: "black" }
+        });
+      }
+      else if (element === 'b' || element === 'g') {
+        // Colt
+        this.board.push({
+          x: i,
+          y: (8 - 8),
+          figure: { name: "savings", color: "black" }
+        });
+      }
+      else if (element === 'c' || element === 'f') {
+        // Runner
+        this.board.push({
+          x: i,
+          y: (8 - 8),
+          figure: { name: "account_circle", color: "black" }
+        });
+      }
+      else if (element === 'd') {
+        // Queen
+        this.board.push({
+          x: i,
+          y: (8 - 8),
+          figure: { name: "grade", color: "black" }
+        });
+      }
+      else if (element === 'e') {
+        // King
+        this.board.push({
+          x: i,
+          y: (8 - 8),
+          figure: { name: "engineering", color: "black" }
+        });
+      }
+    }
   }
 }

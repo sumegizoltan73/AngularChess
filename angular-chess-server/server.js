4532e833dd3136f881a811c45cfa30abfa544258
Sümegi Zoltán <sumegizoltan73@gmail.com>
2021-08-15 19:47:21 +0200

    server with cors

--- angular-chess-server/server.js
+++ angular-chess-server/server.js
@@ -1,6 +1,9 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var cors = require('cors');
+
+app.use(cors({ origin: 'http://localhost:4200' }))
 
 app.get('/', (req, res) => res.send('hello!'));
 

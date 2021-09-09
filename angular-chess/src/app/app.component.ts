328b6c99b86bf70d546620639d49c96d9e38e3c8
Sümegi Zoltán <sumegizoltan73@gmail.com>
2021-09-09 18:32:42 +0200

    browser size-adjusted board

--- angular-chess/src/app/app.component.ts
+++ angular-chess/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-chess';
+
+  @HostListener('window:resize', ['$event'])
+  @HostListener('window:load', ['$event'])
+  onResize(event: Event) {
+    const el = document.querySelector('.board-div');
+    const parentrow = el?.parentNode;
+    let extra = 0;
+    let width = (el?.clientWidth) ? el?.clientWidth : 540;
+    console.log((width) ,(<HTMLDivElement> parentrow).clientWidth, window.innerWidth, '   -----  ', window.innerHeight);
+    if ((width + 250) < (<HTMLDivElement> parentrow).clientWidth) {
+      width = window.innerWidth / 2;
+      extra = 40;
+    }
+    const height = window.innerHeight;
+    const maxValue = (width < (height - 120)) ? width : height - 210;
+    const normalValue = 470 + extra;
+    console.log(maxValue, extra, normalValue);
+    const percentage = parseInt((maxValue / normalValue * 100).toString(), 10);
+
+    console.log(percentage);
+    (<HTMLHtmlElement> document.children[0]).style.fontSize = percentage + '%';
+  }
 }
